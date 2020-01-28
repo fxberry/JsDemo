b3e710e13512282f8a33b57247cc39ed6dded30a
cross equip <code@crossequip.ch>
2020-01-28 19:48:29 +0100

    second commit

---
+++ jsfile.js
@@ -0,0 +1,29 @@
+console.log('hello from console js');
+console.log('1 == "1"');
+console.log(1 == "1");
+console.log('1 == 2');
+console.log(1 == 2);
+console.log('1 === "1"');
+console.log(1 === "1");
+console.log('1 === 1');
+console.log(1 === 1);
+
+// document.write('hello in dom!');
+
+function achtung() {
+    // window.alert('ACHTUNG!');
+    hallo = 3;
+
+}
+
+hallo = function() {
+    return 1*1;
+}
+console.log(typeof hallo);
+
+hallo = "du";
+console.log(typeof hallo);
+
+hallo = true;
+
+console.log(typeof hallo);
